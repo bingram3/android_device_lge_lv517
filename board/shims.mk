2bf68bb1635785f77cbe1c6b9335cca9491c390b
Brian <badfish.brian@gmail.com>
2019-03-27 16:10:16 -0400

    resolve build errors for pie
    Change-Id: Ib88b4d8bb1ed7770cc0bcbe1bd4b72d62a0c92b0

--- board/shims.mk
+++ board/shims.mk
@@ -2,5 +2,5 @@
 TARGET_LD_SHIM_LIBS := \
     /system/lib/hw/camera.msm8937.so|libshim_camera.so \
     /system/lib/libfilm_emulation.so|libshim_camera.so \
-    /system/vendor/lib/hw/camera.msm8937.so|libshim_camera.so \
-    /system/lib/libcameraservice|libshim_cameraservice.so
+    /system/vendor/lib/hw/camera.msm8937.so|libshim_camera.so
+    
